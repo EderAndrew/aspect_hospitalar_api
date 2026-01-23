06e1064d5b32f17ab95c19ce33c8910564c761e4
Eder Andrew <ederandrew0028@gmail.com>
2026-01-23 13:03:36 -0300

    folders user, exams and schdule created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from 'src/user/user.module';
+import { ExamsModule } from 'src/exams/exams.module';
+import { ScheduleModule } from 'src/schedule/schedule.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ExamsModule, ScheduleModule],
   controllers: [AppController],
   providers: [AppService],
 })
