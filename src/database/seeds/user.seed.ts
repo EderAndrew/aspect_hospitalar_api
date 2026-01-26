6b18240c55dee6c6a5fe92a81499caae05696a90
Eder Andrew <ederandrew0028@gmail.com>
2026-01-26 19:36:54 -0300

    Fixing seer:user

--- src/database/seeds/user.seed.ts
+++ src/database/seeds/user.seed.ts
@@ -2,13 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../../app/app.module';
 import { UsersService } from '../../users/users.service';
 import { UserRole } from '../../users/enums/user-role.enum';
-import { HashingService } from '../../auth/hashing/hashing.service';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
 
   const userService = app.get(UsersService);
-  const hashing = app.get(HashingService);
 
   const existUser = await userService.findByEmail(
     process.env.SEED_EMAIL as string,
@@ -19,14 +17,10 @@ async function bootstrap() {
     return;
   }
 
-  const hashedPassword = await hashing.hash(
-    process.env.SEED_PASSWORD as string,
-  );
-
   await userService.create({
     name: process.env.SEED_NAME as string,
     email: process.env.SEED_EMAIL as string,
-    password: hashedPassword,
+    password: process.env.SEED_PASSWORD as string,
     role: process.env.SEED_ROLE as UserRole,
     avatar: process.env.SEED_AVATAR,
     status: Boolean(process.env.SEED_STATUS),
